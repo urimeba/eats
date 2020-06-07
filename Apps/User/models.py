00a1b60b563a625501ef840cbff0b0c06b66c7b9
Andrea  Cecilia López González <lopezgonzalezandi@gmail.com>
2020-06-07 14:55:47 -0500

    prueba x

--- Apps/User/models.py
+++ Apps/User/models.py
@@ -8,10 +8,7 @@ def random_string():
 
 # Create your models here.
 class User(AbstractUser):
-<<<<<<< HEAD
     is_active = models.BooleanField(null=False, blank=False, default=False)
-=======
->>>>>>> b36fa81... *fix* Unique email for each user
     email = models.EmailField(unique=True, null=True, blank=False,verbose_name='correo electronico', max_length=255)
     creditos = models.PositiveIntegerField(default=0, null=False, blank=0)
     is_cafeteria = models.BooleanField(default=False, null=False, blank=False)
