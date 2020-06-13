a8c0d4d9e7a16b4896315aaa5f9a8081e2543207
Uriel Medina <urimeba511@gmail.com>
2020-06-13 10:05:14 -0500

    *fix* SQLite3

--- eats/settings.py
+++ eats/settings.py
@@ -75,6 +75,13 @@ WSGI_APPLICATION = 'eats.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        # CHANGING THE DATABASE SETTINGS
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
