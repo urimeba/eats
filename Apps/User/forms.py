4e228e50f0769ffe314c6dd13cb1cb8530b8205d
Uriel Medina <urimeba511@gmail.com>
2020-08-20 14:00:46 -0500

    *fix* Commented LoginViewForm

--- Apps/User/forms.py
+++ Apps/User/forms.py
@@ -31,28 +31,28 @@ class LoginForm(AuthenticationForm):
             'placeholder':'Contraseña'
             }))
 
-    def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
+    # def clean(self):
+    #     username = self.cleaned_data.get('username')
+    #     password = self.cleaned_data.get('password')
 
-        if username is not None and password:
-            self.user_cache = authenticate(self.request, username=username, password=password)
-            if self.user_cache is None:
-                try:
-                    user_temp = User.objects.get(username=username)
-                except:
-                    user_temp = None
+    #     if username is not None and password:
+    #         self.user_cache = authenticate(self.request, username=username, password=password)
+    #         if self.user_cache is None:
+    #             try:
+    #                 user_temp = User.objects.get(username=username)
+    #             except:
+    #                 user_temp = None
 
-                if user_temp is not None:
-                        self.confirm_login_allowed(user_temp)
-                else:
-                    raise forms.ValidationError(
-                        self.error_messages['invalid_login'],
-                        code='invalid_login',
-                        params={'username': self.username_field.verbose_name},
-                    )
+    #             if user_temp is not None:
+    #                     self.confirm_login_allowed(user_temp)
+    #             else:
+    #                 raise forms.ValidationError(
+    #                     self.error_messages['invalid_login'],
+    #                     code='invalid_login',
+    #                     params={'username': self.username_field.verbose_name},
+    #                 )
 
-        return self.cleaned_data
+    #     return self.cleaned_data
 
 class SignupForm(UserCreationForm):
     username = forms.CharField(
